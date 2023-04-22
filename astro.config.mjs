3d6b060318d0e75a544a04d1ff78137ba3ddf1da
Sergio Campbell Dev <info.sergiocampbell@gmail.com>
2023-04-22 17:29:03 -0500

    config: changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://sergiocampbell.github.io',
-  base: 'https://github.com/SergioCampbell',
+  site: "https://sergiocampbell.github.io",
+  root: "."
 });
