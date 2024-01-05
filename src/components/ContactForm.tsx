8a1a4ce06ba6558ff6354274ff67bb6bfb335904
Sergio Campbell Dev <info.sergiocampbell@gmail.com>
2024-01-05 14:04:04 +0100

    chore: adjust email parameters

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -2,16 +2,10 @@
 import React, { useRef } from 'react';
 import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 
-interface FormData {
-    subject: string;
-    email: string;
-    message: string;
-}
-
 export default function ContactForm() {
-const form = useRef<HTMLFormElement>(null);
+const form = useRef();
 const [wasSended, setWasSended] = React.useState(false)
-  const submit = (e: any) => {
+  const submit = (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
     emailjs.sendForm("service_ztojqk4", "template_n0zrrmg",
         form.current, "user_rB8qWaGWX9OFw575diXB3")
@@ -28,19 +22,19 @@ return(
     {!wasSended ?
     <form ref={form} className='contact' onSubmit={submit}>
         <label>
-            <span>Subject</span><br />
-            <input className='contactInput' type="text" name="subject" placeholder="Subject email" required />
+            <span>Name</span><br />
+            <input className='contactInput' type="text" name="nameEmail" placeholder="Your name" required />
 
         </label>
         <label>
             <span>Email</span><br />
-            <input className='contactInput' required name="email" type="email" placeholder='Email address'
+            <input className='contactInput' required name="userEmail" type="email" placeholder='Email address'
         />
 
         </label>
         <label>
             <span>Message</span><br />
-            <textarea className='contactTextarea' required name="message" placeholder="Your message"></textarea>
+            <textarea className='contactTextarea' required name="emailDetails" placeholder="Your message"></textarea>
 
         </label>
       <br/>
